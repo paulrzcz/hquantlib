6f0545c6a6220bc1837d9aaf1b2301d58a5c751f
Paul <paul@paulrz.cz>
2012-05-28 15:02:29 +0200

    Copulas refactored and structured

--- src/QuantLib/Math.hs
+++ src/QuantLib/Math.hs
@@ -1,7 +1,7 @@
 module QuantLib.Math
         ( module QuantLib.Math.InverseNormal
-        , module QuantLib.Math.ClaytonCopula
+        , module QuantLib.Math.Copulas
         ) where
 
 import QuantLib.Math.InverseNormal
-import QuantLib.Math.ClaytonCopula
+import QuantLib.Math.Copulas
---
+++ src/QuantLib/Math/Copulas.hs
@@ -0,0 +1,73 @@
+module QuantLib.Math.Copulas
+        ( Copula,
+          Copulas (..) 
+        ) where
+
+{-| Copula type class. 
+ -| Normally instance should implement only copulaFunc.
+ -| Method copula provides a precheck for [0..1] range for x and y but real implementation is in copulaFunc
+ -}
+class Copula a where
+        copula :: a -> Double -> Double -> Maybe Double
+        copula t = precheckRange (copulaFunc t)
+        copulaFunc :: a -> Double -> Double -> Maybe Double
+
+-- Copula must be in [0,1] range
+precheckRange :: (Double->Double->Maybe Double) -> Double -> Double -> Maybe Double
+precheckRange f x y
+        | x >= 0.0 || x <= 1.0
+        || y>= 0.0 || y <= 1.0
+                = f x y
+        | otherwise
+                = Nothing
+
+{-| Copula data types with parameters required by the concrete copula definition
+ -}
+data Copulas = ClaytonCopula Double
+        | MinCopula
+        | MaxCopula
+        | AliMikhailHaqCopula Double
+        | FarlieGumbelMorgensternCopula Double
+
+instance Copula Copulas where
+        copulaFunc (ClaytonCopula theta) = claytonCopula theta
+        copulaFunc MinCopula = minCopula
+        copulaFunc MaxCopula = maxCopula
+        copulaFunc (AliMikhailHaqCopula theta) = aliMikhailHaqCopula theta
+        copulaFunc (FarlieGumbelMorgensternCopula theta) = farlieGumbelMorgenstern theta
+
+{- Private implementations   -}
+
+aliMikhailHaqCopula :: (Fractional a, Ord a) => a -> a -> a -> Maybe a
+aliMikhailHaqCopula theta x y
+        | theta >= -1.0 && theta <= 1.0
+                = Just ((x*y)/(1.0 - theta * (1.0-x)*(1.0-y)))
+        | otherwise
+                = Nothing
+
+farlieGumbelMorgenstern :: (Fractional a, Ord a) => a -> a -> a -> Maybe a
+farlieGumbelMorgenstern theta x y
+        | theta >= -1.0 && theta <= 1.0
+                = Just (x*y + theta*x*y*(1.0-x)*(1.0-y))
+        | otherwise
+                = Nothing
+
+{-|  Original code and algorithm from the Quantlib project
+     implemented in Haskell by Nicholas Pezolano 
+                                  npezolano "at" gmail.com
+-}
+claytonCopula :: Double -> Double -> Double -> Maybe Double
+claytonCopula theta x y
+	|  theta ==0 
+	|| theta < -1.0
+	 = Nothing
+
+	| otherwise 
+	= Just $ max( (x ** (-theta) ) +   (y ** (-theta)-1.0)  **   (-1.0/theta)) 0
+	
+minCopula ::  Ord a => a -> a -> Maybe a
+minCopula x y = Just (min x y)
+
+maxCopula ::  (Fractional a, Ord a) => a -> a -> Maybe a
+maxCopula x y = Just (max 0.0 (x+y-1.0))
+
